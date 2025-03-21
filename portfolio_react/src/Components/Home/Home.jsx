a9e96a56614620b128147c0c8d61285863f95b59
Aamir Ahmad <aamir557ahmad@gmail.com>
2025-03-22 04:04:22 +0530

    Home section\ Added

--- portfolio_react/src/Components/Home/Home.jsx
+++ portfolio_react/src/Components/Home/Home.jsx
@@ -1,15 +1,12 @@
 import React from "react";
 import Me from "../../assets/Aamir Nobg.png";
-import yellow from '../../assets/blob.svg'
-import blue from '../../assets/blue.svg'
 
 const Home = () => {
   return (
     <div
       className="text-white w-full p-10 md:p-20"
       style={{
-        background:
-          "linear-gradient(90deg, rgba(34,3,34,1) 26%, rgba(64,2,64,1) 68%)",
+        background: "linear-gradient(90deg, rgba(34,3,34,1) 26%, rgba(64,2,64,1) 68%)",
       }}
     >
       <div className="flex flex-col md:flex-row justify-between items-center gap-10">
@@ -19,47 +16,22 @@ const Home = () => {
             Hello, I am Aamir
           </h1>
           <p className="text-sm md:text-2xl mt-4 tracking-tight">
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Vero
-            aperiam quisquam rerum itaque reiciendis ipsum.
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Vero aperiam quisquam rerum itaque reiciendis ipsum.
           </p>
           <button className="mt-10 bg-gradient-to-r from-yellow-400 to-orange-500 text-white text-xl font-bold py-3 px-6 rounded-full shadow-lg transform transition-all duration-300 hover:scale-105 hover:shadow-[0_10px_20px_rgba(255,165,0,0.5)] hover:from-orange-500 hover:to-yellow-400">
             Contact Me
           </button>
         </div>
 
-        {/* Right: Image with SVG Blobs */}
-        <div className="relative w-full flex justify-center items-center">
-          {/* Yellow Blob */}
-          <svg
-            className="absolute w-72 md:w-96 z-0 top-1/2 left-1/2 transform -translate-x-[60%] -translate-y-1/2"
-            viewBox="0 0 200 200"
-            xmlns={yellow}
-          >
-            <path
-              fill="#FACC15"
-              d="M44.8,-72.9C57.3,-66.6,66.9,-57.3,73.6,-45.9C80.2,-34.4,84,-20.7,83.2,-7.2C82.4,6.2,77.1,19.4,69.2,30.4C61.2,41.5,50.7,50.3,39,56.2C27.4,62.1,13.7,65.2,0.1,65C-13.5,64.8,-27,61.3,-39.6,55.3C-52.2,49.2,-63.8,40.6,-71.6,29.2C-79.4,17.8,-83.5,3.6,-80.5,-10.1C-77.6,-23.7,-67.6,-36.8,-56.2,-44.9C-44.8,-53.1,-32.1,-56.4,-19.3,-62.4C-6.4,-68.4,6.6,-77.2,20.6,-80.2C34.6,-83.2,49.4,-80.1,44.8,-72.9Z"
-              transform="translate(100 100)"
-            />
-          </svg>
-
-          {/* Blue Blob */}
-          <svg
-            className="absolute w-72 md:w-96 z-0 top-1/2 left-1/2 transform -translate-x-[40%] -translate-y-1/2"
-            viewBox="0 0 200 200"
-            xmlns={blue}
-          >
-            <path
-              fill="#3B82F6"
-              d="M44.8,-72.9C57.3,-66.6,66.9,-57.3,73.6,-45.9C80.2,-34.4,84,-20.7,83.2,-7.2C82.4,6.2,77.1,19.4,69.2,30.4C61.2,41.5,50.7,50.3,39,56.2C27.4,62.1,13.7,65.2,0.1,65C-13.5,64.8,-27,61.3,-39.6,55.3C-52.2,49.2,-63.8,40.6,-71.6,29.2C-79.4,17.8,-83.5,3.6,-80.5,-10.1C-77.6,-23.7,-67.6,-36.8,-56.2,-44.9C-44.8,-53.1,-32.1,-56.4,-19.3,-62.4C-6.4,-68.4,6.6,-77.2,20.6,-80.2C34.6,-83.2,49.4,-80.1,44.8,-72.9Z"
-              transform="translate(100 100)"
-            />
-          </svg>
-
-          {/* Actual Image */}
+        {/* Right: Image with Blobs */}
+        <div className="relative w-full md:w-1/2 flex justify-center items-center">
+          <div className="absolute w-72 h-72 bg-blue-500 top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 blur-2xl opacity-80 z-0" />
+          <div className="absolute w-72 h-72 bg-yellow-400 rounded-[8vw] top-1/2 left-[45%] transform -translate-x-1/2 -translate-y-1/2 rotate-45 blur-2xl opacity-80 z-0" />
+          {/* Image */}
           <img
             src={Me}
             alt="My Photo"
-            className="relative z-10 w-72 md:w-96 object-cover"
+            className="relative z-10 w-72 md:w-96 object-contain"
           />
         </div>
       </div>
